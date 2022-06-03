1aead16b6a9c78432db4c6cdf10c1b91f7fb050b
jjbreezy <46391151+jjbreezy@users.noreply.github.com>
2022-06-03 12:04:51 -0700

    changed deck reference

--- js/cardgamejs.js
+++ js/cardgamejs.js
@@ -20,7 +20,7 @@
             
             var jsontestdeck = [];
             //JSON test deck fetch
-            fetch("/decks/betadeck.json")
+            fetch("../cleandeck.json")
                 .then(response => response.json())
                 .then(data => {
                     jsontestdeck = data;
